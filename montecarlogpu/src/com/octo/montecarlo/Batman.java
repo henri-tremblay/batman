879f3b90e2730bd22bd578d0cb9d84a5ddf27420
Henri Tremblay <henri.tremblay@gmail.com>
2012-09-03 01:20:11 +0200

    Version optimized and with a real random

--- montecarlogpu/src/com/octo/montecarlo/Batman.java
+++ montecarlogpu/src/com/octo/montecarlo/Batman.java
@@ -8,96 +8,104 @@ import com.amd.aparapi.Range;
 
 public class Batman {
 
-    private static int ELEMENTS = 1024;
-    private static int ELEMENTS_PER_WORKER = 8192;
-            
     public static class BatmanKernel extends Kernel {
+
+        static final int multiplier = 3559;
+
+        static final int addend = 0xB;
+
+        static final int mask = (1 << 24) - 1;
 
         final float width = 10;
 
         final float height = 5;
 
-        int elements, elementsPerWorker;
+        int size;
 
         int[] result;
 
-        float[] randx, randy;
+        int[] seed;
+        int[] temp;
 
         public boolean f(float x, float y) {
+            
             // Wings bottom
-            if (pow(x, 2.0f) / 49.0f + pow(y, 2.0f) / 9.0f - 1.0f <= 0 && abs(x) >= 4.0f && -(3.0f * sqrt(33.0f)) / 7.0f <= y && y <= 0) {
-                return true;
-            }
-            // Wings top (with the fix of the formula there are missing parenthesis
-            // in the original
-            if (pow(x, 2.0f) / 49.0f + pow(y, 2.0f) / 9.0f - 1.0f <= 0 && abs(x) >= 3.0f && -(3.0f * sqrt(33.0f)) / 7.0f <= y && y >= 0) {
-                return true;
-            }
+            boolean result = (pow(x, 2.0f) / 49.0f + pow(y, 2.0f) / 9.0f - 1.0f <= 0 && abs(x) >= 4.0f
+                    && -(3.0f * sqrt(33.0f)) / 7.0f <= y && y <= 0);
+
+            // Wings top (with the fix of the formula there are missing parenthesis in the original)
+            result |= (pow(x, 2.0f) / 49.0f + pow(y, 2.0f) / 9.0f - 1.0f <= 0 && abs(x) >= 3.0f
+                    && -(3.0f * sqrt(33.0f)) / 7.0f <= y && y >= 0);
+
             // Tail
-            if (-3.0f <= y
-                    && y <= 0
-                    && -4.0f <= x
-                    && x <= 4.0f
-                    && (abs(x)) / 2.0f + sqrt(1.0f - pow(abs(abs(x) - 2.0f) - 1.0f, 2.0f)) - 1.0f / 112.0f * (3.0f * sqrt(33.0f) - 7.0f)
-                            * pow(x, 2.0f) - y - 3.0f <= 0) {
-                return true;
-            }
+            result |= (-3.0f <= y && y <= 0 && -4.0f <= x && x <= 4.0f && (abs(x)) / 2.0f
+                    + sqrt(1.0f - pow(abs(abs(x) - 2.0f) - 1.0f, 2.0f)) - 1.0f / 112.0f * (3.0f * sqrt(33.0f) - 7.0f)
+                    * pow(x, 2.0f) - y - 3.0f <= 0);
+
             // Ears outside
-            if (y >= 0 && 3.0f / 4.0f <= abs(x) && abs(x) <= 1.0f && -8.0f * abs(x) - y + 9.0f >= 0) {
-                return true;
-            }
+            result |= (y >= 0 && 3.0f / 4.0f <= abs(x) && abs(x) <= 1.0f && -8.0f * abs(x) - y + 9.0f >= 0);
+
             // Ears inside
-            if (1.0f / 2.0f <= abs(x) && abs(x) <= 3.0f / 4.0f && 3.0f * abs(x) - y + 3.0f / 4.0f >= 0 && y >= 0) {
-                return true;
-            }
-            if (abs(x) <= 1.0f / 2.0f && y >= 0 && 9.0f / 4.0f - y >= 0) {
-                return true;
-            }
+            result |= (1.0f / 2.0f <= abs(x) && abs(x) <= 3.0f / 4.0f && 3.0f * abs(x) - y + 3.0f / 4.0f >= 0 && y >= 0);
+
+            // Chest
+            result |= (abs(x) <= 1.0f / 2.0f && y >= 0 && 9.0f / 4.0f - y >= 0);
+
             // Shoulders
-            if (abs(x) >= 1.0f
-                    && y >= 0
-                    && -(abs(x)) / 2.0f - 3.0f / 7.0f * sqrt(10.0f) * sqrt(4.0f - pow(abs(x) - 1.0f, 2.0f)) - y + (6.0f * sqrt(10.0f))
-                            / 7.0f + 3.0f / 2.0f >= 0) {
-                return true;
-            }
-            return false;
+            result |= (abs(x) >= 1.0f && y >= 0 && -(abs(x)) / 2.0f - 3.0f / 7.0f * sqrt(10.0f)
+                    * sqrt(4.0f - pow(abs(x) - 1.0f, 2.0f)) - y + (6.0f * sqrt(10.0f)) / 7.0f + 3.0f / 2.0f >= 0);
+
+            return result;
+        }
+
+        public float nextFloat(int index) {
+            return next(index) / ((float) (1 << 24));
+        }
+
+        private int next(int index) {
+            int seed = this.seed[index];
+            int nextseed = (seed * multiplier + addend) & mask;
+            this.seed[index] = nextseed;
+            return nextseed;
         }
 
         @Override
         public void run() {
             int index = getGlobalId();
-
-            for(int i = 0; i < elementsPerWorker; i++) {
-                float x = randx[index * elements + i];
-                float y = randy[index * elements + i];
-                if(f(x, y)) {
-                    result[index]++;
-                }
+            float x = nextFloat(index) * width * 2.0f - width;
+            float y = nextFloat(index) * height * 2.0f - height;
+            temp[index]++;
+            if(f(x, y)) {
+                result[index]++;
             }
         }
 
-        public BatmanKernel(int elements, int elementsPerWorker) {
-            this.elements = elements;
-            this.elementsPerWorker = elementsPerWorker;
-            result = new int[elements];
-            randx = new float[elements * elementsPerWorker];
-            randy = new float[elements * elementsPerWorker];
-            
+        public BatmanKernel(int size) {
+            this.size = size;
+        }
+
+        public void init() {
+
+            result = new int[size];
             Arrays.fill(result, 0);
-            Random rand = new Random();
+
+            seed = new int[size];
+            temp = new int[size];
+            Arrays.fill(temp, 0);
             
-            for (int i = 0; i < elements * elementsPerWorker; i++) {
-                randx[i] = rand.nextFloat() * width * 2.0f - width;
-                randy[i] = rand.nextFloat() * height * 2.0f - height;
+            Random rand = new Random(1);
+            for (int i = 0; i < size; i++) {
+                seed[i] = rand.nextInt();
             }
         }
 
-        public void showResult() {
+        public void showResult(int passes) {
             int p = 0;
-            int n = elements * elementsPerWorker;
+            int n = size * passes;
             for (int i = 0; i < result.length; i++) {
                 p += result[i];
             }
+            
             float finalResult = 4.0f * width * height * p / n;
             System.out.println("Final result: " + finalResult);
             System.out.println("Iterations: " + n);
@@ -106,17 +114,26 @@ public class Batman {
 
     public static void main(String[] _args) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
 
-        int elements = Integer.getInteger("elements", ELEMENTS);
-        Range range = Range.create(elements);
-        int elementsPerWorker = Integer.getInteger("elementsPerWorker", ELEMENTS_PER_WORKER);
-        System.out.println("elements =" + elements);
-        System.out.println("elementsPerWorker =" + elementsPerWorker);
-        BatmanKernel kernel = new BatmanKernel(elements, elementsPerWorker);
+        int size = Integer.getInteger("size", 4096);
+        int iterations = Integer.getInteger("iterations", 20000);
+        
+        System.out.printf("size = %d%n", size);
+        System.out.printf("iterations = %d%n", iterations);
+        System.out.println();
 
-        kernel.execute(range);
-        System.out.println("Execution mode: " + kernel.getExecutionMode());
-        System.out.println("Execution time " + kernel.getAccumulatedExecutionTime());
-        kernel.showResult();
+        Range range = Range.create(size);
+        
+        BatmanKernel kernel = new BatmanKernel(size);
+        kernel.init();
+        
+        long start = System.currentTimeMillis();
+        kernel.execute(range, iterations);
+        long stop = System.currentTimeMillis();
+        
+        System.out.printf("Execution mode: %s%n", kernel.getExecutionMode());
+        System.out.printf("Time: %d ms%n", (stop - start));
+        kernel.showResult(iterations);
+        
         kernel.dispose();
     }
 
